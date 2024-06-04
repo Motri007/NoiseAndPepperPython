b0f59da2b6e163277b86ea35ab7a9744df498e75
Motasem Akbari <sonic.speed757@gmail.com>
2024-06-04 23:27:23 +0330

    Update NoseAndPepper.py

--- NoseAndPepper.py
+++ NoseAndPepper.py
@@ -13,25 +13,32 @@ cv2.destroyAllWindows()
 
 
 x,y = imageBW.shape
-g = np.zeros((x,y) , dtype= np.float32)
+a = np.zeros((x,y) , dtype= np.float32)
 
+# salt and pepper
+# nose 10% pepper=0.5
+# nose 25% pepper=0.125
+# nose 50% pepper=0.25
 pepper = 0.25
 salt = 1 - pepper
 
+# sakht salt and pepper
 for i in range(x):
     for j in range(y):
         rnd=np.random.random()
         if rnd < pepper:
-            g[i][j] = 0
+            a[i][j] = 0
         elif rnd > salt:
-            g[i][j] = 1
+            a[i][j] = 1
         else:
-            g[i][j] = imageBW[i][j]
+            a[i][j] = imageBW[i][j]
 
-image_noisy = g
+
+image_noisy = a
 cv2.imshow('image noisy', image_noisy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# save image
 output_path = "C:\\Users\\Motri\\Documents\\GitHub\\NoiseAndPepperPython\\tasvir_noisy.jpg"
 cv2.imwrite(output_path, image_noisy)
